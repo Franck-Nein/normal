5e5ddcf289eb926c9d49eae8fa6146586136a0e6
Franck-Nein <franco600@live.com.ar>
2021-04-01 11:30:24 +0000

    t

--- overviewerConfig.js
+++ overviewerConfig.js
@@ -27,7 +27,7 @@ var overviewerConfig = {
     ],
     "map": {
         "debug": false,
-        "cacheTag": "1617247616",
+        "cacheTag": "1617276621",
         "north_direction": "lower-left",
         "controls": {
             "pan": true,
